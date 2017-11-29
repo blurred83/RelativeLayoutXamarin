d68d6416c107d9e9acc3691a03ac161ddb9ff8f5
Max Szczurek <max.szczurek@gmail.com>
2017-11-29 00:50:44 -0600

    fc

--- App4/App4/MainPage.xaml.cs
+++ App4/App4/MainPage.xaml.cs
@@ -12,6 +12,26 @@ namespace App4
         public MainPage()
         {
             InitializeComponent();
+
+            var employees = new List<Employee>();
+            employees.Add(new Employee { DisplayName = "Brian Williams" });
+            employees.Add(new Employee { DisplayName = "Mindy Collette" });
+            employees.Add(new Employee { DisplayName = "Tami Taylor" });
+            employees.Add(new Employee { DisplayName = "Eric Taylor" });            
+            employees.Add(new Employee { DisplayName = "Landry Clarke" });
+            employees.Add(new Employee { DisplayName = "Vince Howard" });
+            
+            EmployeeView.ItemsSource = employees;
+
         }
+
+        private void EmployeeView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            EmployeeView.SelectedItem = null;
+        }
+    }
+    public class Employee
+    {
+        public string DisplayName { get; set; }
     }
 }
